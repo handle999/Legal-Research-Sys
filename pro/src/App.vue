e741f71e72f53bfcb7cca0aa0c797daf0e813787
handle1024 <1120201179@bit.edu.cn>
2024-02-22 21:32:31 +0800

    update vue

--- pro/src/App.vue
+++ pro/src/App.vue
@@ -83,3 +83,13 @@ nav a:first-of-type {
   }
 }
 </style>
+// main.ts
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
