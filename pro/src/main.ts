40fd320ba175bb247422007086f1f6371be40a17
hhy <1833132531@qq.com>
2024-02-23 21:33:51 +0800

    add cascader

--- pro/src/main.ts
+++ pro/src/main.ts
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
 
@@ -8,4 +11,5 @@ const app = createApp(App)
 
 app.use(router)
 
+app.use(ElementPlus)
 app.mount('#app')
